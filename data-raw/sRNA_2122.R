66b7413aff995fcd561985f0db39f2010f460555
KJeynesCupper <kej031@student.bham.ac.uk>
2023-06-08 08:58:42 +0100

    ignores

--- data-raw/sRNA_2122.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `sRNA_2122` dataset goes here
-
-# this creates a dataframe from summary data which only contains sRNA with a length og either 21 or 22 nucleotides.
-sRNA_2122 <- RNAsubset(sRNA_data_summary, type = c(21, 22), sig = FALSE)
-
-usethis::use_data(sRNA_2122, overwrite = TRUE)
--- data-raw/sRNA_24.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `sRNA_24` dataset goes here
-
-# This is a dataframe containing a subset of the summary data, only includinh the sRNAs which have a consensus of 24-nt.
-# Subset data: 24-nt siRNAs
-sRNA_24 <- RNAsubset(sRNA_data_summary, type = 24, sig = FALSE)
-usethis::use_data(sRNA_24, overwrite = TRUE)
