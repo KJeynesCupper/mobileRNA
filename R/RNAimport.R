09d585988b0344034b82326892c1e89c6905af47
Katie Jeynes-Cupper <77631989+KJeynesCupper@users.noreply.github.com>
2023-08-14 20:13:33 +0100

    Update RNAimport.R

--- R/RNAimport.R
+++ R/RNAimport.R
@@ -205,7 +205,14 @@ RNAimport <- function(input = c("sRNA", "mRNA"), directory, samples,
                                      after = 4))
 
 
-
+    
+# Remove rows with no counts 
+count_columns <- grep("^Count", names(df_final))
+# Identify rows where all values in Count columns are zero
+rows_to_remove <- apply(df_final[count_columns], 1, function(row) all(row == 0))
+# Remove rows with all zero values in Count columns
+df_final <- df_final[!rows_to_remove, ]   
+                        
     # return values
     return(df_final)
 
